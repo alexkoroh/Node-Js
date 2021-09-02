a2e634f7c8b4f09ac56b7c9a189671c24ffe9a30
Alexander Bryand Koroh <75639365+alexkoroh@users.noreply.github.com>
2021-09-02 19:57:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 
 // kita membuat sebuah fungsi yang bernama hitungKecepatan yangf memilki parameter j dan w;
 function hitungKecepatan(j, w) {
+    
     // var kecepatan berisi nilai awal dari parametre j;
     var kecepatan = j;
+    
     // cost c menampung hasil dari 15 / 60;
     var c = (15 / 60);
+    
     // kemudian haisl dari j bagi dengan c di tampung dalam variabel hasil;
     var hasil = j / c
 
     //Menampilkan variabel hasil ke layar
     console.log(`hasil =` + hasil + ' km/jam');
 }
+
 // memanggil fungsi di atas.....
 console.log(hitungKecepatan(7, 15));
 
@@ -27,10 +31,13 @@ console.log(hitungKecepatan(7, 15));
 
 // kita membuat sebuah fungsi dengan nama perceoatanRataRata dengan memiliki pareameter x,y,t;
 function percepatanRataRata(x, y, t) {
+    
     // var a menampung hasil sari parameter y - x;
     var a = y - x;
+    
     //var haisl menampung a /t;
     var haisl = a / t;
+    
     //Menampilkan hasil dari proses di atas yang di tampung oleh parameter haisl
     console.log(`hasil =` + haisl + " m/s");
 
