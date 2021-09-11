f32efe1b1f594041a5a7c4b2f722361513686ca1
Alexander Bryand Koroh <75639365+alexkoroh@users.noreply.github.com>
2021-09-11 20:01:03 +0800

    Add files via upload

---
+++ soal2.js
@@ -0,0 +1,25 @@
+var s = '';
+
+for( var i = 10; i > 1; i--){
+    for( var j = 1; j < i; j++){
+        s+=j;
+    }
+    s +='\n'
+}
+console.log(s);
+var k ='';
+var no = 10;
+//-----------------------------------------------------------
+for (var c = 1; c < 10 ; c++){
+    no -=1;
+    for(var d = 0 ; d <= c -1;  d++){
+        k += `*`;
+        
+       
+
+    }
+    k+= no
+    k += '\n'
+}
+
+console.log(k);
