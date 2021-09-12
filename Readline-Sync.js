05f575e73dc9114472fbc4bcf2370230342c1cde
Alexander Bryand Koroh <75639365+alexkoroh@users.noreply.github.com>
2021-09-12 12:02:02 +0800

    Add files via upload

---
+++ Readline-Sync.js
@@ -0,0 +1,28 @@
+const readline = require(`readline-sync`)
+
+const IP = parseFloat(readline.question("Masukan IP anda :"))
+
+
+PinPrestasi = (IP) =>{
+
+    if( IP == 4.00){
+        console.log("Platinum");
+    }else if ( IP >= 3.85 && IP <= 3.99){
+        console.log("Gold");
+    }else if( IP >= 3.75 && IP <= 3.84){
+        console.log("Silver");
+    }else if( IP >= 3.00 && IP <= 3.74){
+        console.log("Bronze");
+    }else if( IP < 3.00 && IP != 0){
+        console.log("Reguler");
+    }else if( IP == 0){
+        console.log("Basic");
+    }else{
+        console.log("Maaf Anda salah input IP !!!");
+    }
+       
+
+
+}
+
+PinPrestasi(IP)
